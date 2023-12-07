f970ba27c2cca8642b7d368caa6ab96ab964e35c
rajsekhar222 <rajsekhar.s@turtil.co>
2023-12-07 15:20:33 +0530

    added new tf  files1

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "turtilbacendtf"
+    key    = "turtlebackend.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+
 provider "aws" {
   region = "ap-south-1"
 }
