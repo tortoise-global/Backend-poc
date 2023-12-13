35eb133d6b66333a06c3e8efd9a70072bcf160db
rajsekhar222 <rajsekhar.s@turtil.co>
2023-12-13 13:36:06 +0530

    added custom-domain.tf file

---
+++ custom-domain.tf
@@ -0,0 +1,90 @@
+
+#create TLS certificate.
+
+resource "aws_acm_certificate" "api" {
+  domain_name       = "devapi.turtil.co" //give end-point or backend url name (TLS certificate)
+  validation_method = "DNS"
+}
+
+data "aws_route53_zone" "public" {
+  name         = "turtil.co" // give your domain name
+  private_zone = false
+}
+
+resource "aws_route53_record" "api_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.public.zone_id
+}
+
+resource "aws_acm_certificate_validation" "api" {
+  certificate_arn         = aws_acm_certificate.api.arn
+  validation_record_fqdns = [for record in aws_route53_record.api_validation : record.fqdn]
+}
+
+
+
+
+# Specify the domain name, which should match the certificate i.e above given TLS certificate(for our case it is devapi.turtil.co)
+
+resource "aws_apigatewayv2_domain_name" "api" {
+  domain_name = "devapi.turtil.co"
+
+  domain_name_configuration {
+    certificate_arn = aws_acm_certificate.api.arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+
+  depends_on = [aws_acm_certificate_validation.api]
+}
+
+resource "aws_route53_record" "api" {
+  name    = aws_apigatewayv2_domain_name.api.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.public.zone_id
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.api.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.api.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
+
+#let's create API mapping. The first one is for the base path. And the second one is to map the staging stage with the v1 path.
+
+resource "aws_apigatewayv2_api_mapping" "api" {
+  api_id      = aws_apigatewayv2_api.main.id
+  domain_name = aws_apigatewayv2_domain_name.api.id
+  stage       = aws_apigatewayv2_stage.dev.id
+}
+
+resource "aws_apigatewayv2_api_mapping" "api_v1" {
+  api_id          = aws_apigatewayv2_api.main.id
+  domain_name     = aws_apigatewayv2_domain_name.api.id
+  stage           = aws_apigatewayv2_stage.dev.id
+  api_mapping_key = "v1"
+}
+
+output "custom_domain_api" {
+  value = "https://${aws_apigatewayv2_api_mapping.api.domain_name}"
+}
+
+output "custom_domain_api_v1" {
+  value = "https://${aws_apigatewayv2_api_mapping.api_v1.domain_name}/${aws_apigatewayv2_api_mapping.api_v1.api_mapping_key}"
+}
+
+
