cce784327cbdd7a5f0af96fb4bfcb9428f124e19
rajsekhar222 <rajsekhar.s@turtil.co>
2023-12-17 22:31:29 +0530

    added cognito

--- api-gateway.tf
+++ api-gateway.tf
@@ -91,8 +91,8 @@ resource "aws_apigatewayv2_route" "get_BACKEND-POC" {
   api_id        = aws_apigatewayv2_api.main.id
   route_key     = "GET /allpost"
   target        = "integrations/${aws_apigatewayv2_integration.lambda_BACKEND-POC.id}"
-  
-  //authorization_scopes = ["openid"]  # Set the required scopes for authorization
+  authorizer_type    = "JWT"
+  authorization_scopes = ["openid"]  # Set the required scopes for authorization
   authorizer_id        = aws_apigatewayv2_authorizer.cognito_authorizer.id
 }
 
