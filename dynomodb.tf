be17a6e560e94dc5889f40bc77cc520465bb208f
rajsekhar222 <rajsekhar.s@turtil.co>
2023-12-28 08:51:54 +0530

    checking rollback 7

--- dynomodb.tf
+++ dynomodb.tf
@@ -50,7 +50,7 @@ resource "aws_dynamodb_table" "post" {
 }
 
 
-
+/*
 
 resource "aws_dynamodb_table" "testingrollback" {
   name           = "testingrollback"
@@ -65,5 +65,5 @@ resource "aws_dynamodb_table" "testingrollback" {
   hash_key = "rollbackid"
 
 }
-
+*/
 
